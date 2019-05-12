51896d1502b576731fad56c60f4a92f601cc814d
Kadir Yüksel <kadir.yuksel@outlook.com.tr>
2019-05-12 18:55:24 +0300

    Yeni Fonksiyonlar
    ÇALIŞAN FONKSİYONLAR
    -Kullanıcı Ekleme
    -Sorgudan çekilen verileri tabloda gösterme
    -Tablodaki özelliklerden bir tanesinin  değerini değiştrime

--- src/MySQLJDBCUtil.java
+++ src/MySQLJDBCUtil.java
@@ -1,9 +1,7 @@
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+
 
 public class MySQLJDBCUtil {
 
